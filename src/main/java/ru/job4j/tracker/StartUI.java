0f20b42778659d34dd82134a3e7f086663ff5aa5
Artyrio226 <artur_sar_80@mail.ru>
2023-04-16 13:50:27 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,5 +8,6 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTime = item.getCreated().format(formatter);
         System.out.println("Текущие дата и время: " + currentDateTime);
+        System.out.println(item);
     }
 }
