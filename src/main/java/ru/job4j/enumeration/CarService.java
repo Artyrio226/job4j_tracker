517d84b42da8329d96aea451ee82e6000559251c
Artyrio226 <artur_sar_80@mail.ru>
2023-04-16 16:40:30 +0300

    12. Enum  [#504960]

---
+++ src/main/java/ru/job4j/enumeration/CarService.java
@@ -0,0 +1,25 @@
+package ru.job4j.enumeration;
+
+public class CarService {
+    public static void main(String[] args) {
+        Status toyota = Status.FINISHED;
+        Status volvo = Status.WAITING;
+        System.out.println("Статус Toyota: " + toyota);
+        System.out.println("Статус Volvo: " + volvo);
+        Order order = new Order(1, "Mercedes-Benz GLS", Status.IN_WORK);
+        System.out.println("Заказ-наряд №" + order.getNumber() + " на автомобиль " + order.getCar()
+                           + ", статус ремонта: " + order.getStatus());
+        Order order1 = new Order(1, "Mercedes-Benz GLS", Status.IN_WORK);
+        System.out.println("Заказ-наряд №" + order1.getNumber() + " на автомобиль " + order1.getCar()
+                           + ", статус ремонта: " + order1.getStatus().getInfo());
+        Order order2 = new Order(1, "Mercedes-Benz GLS", Status.IN_WORK);
+        System.out.println("Заказ-наряд №" + order2.getNumber()
+                           + " на автомобиль " + order2.getCar()
+                           + ", статус ремонта: " + order2.getStatus().getInfo()
+                           + ", подробности: " + order2.getStatus().getMessage());
+        Status[] statuses = Status.values();
+        for (Status s : statuses) {
+            System.out.println("Название статуса: " + s.name() + ", Порядковый номер статуса: " + s.ordinal());
+        }
+    }
+}
